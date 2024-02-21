a743617afa394fa241105b259ac0e7cc49356a70
pzawadzki <pzawadzki@TE-GDA-D-D36G18>
2024-02-21 13:24:21 +0100

    camera controll done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,10 @@ public class CameraController : MonoBehaviour
     Transform player;
     // offset kamery
     Vector3 cameraOffset;
+    //prêdkoœæ przesuwania kamery - nie nadajemy wartoœci - zrobi to smoothDamp
+    Vector3 cameraSpeed;
+    //czas wyg³adzania ruchu kamery
+    float smoothTime = 0.3f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,18 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         //pozycja kamery to pozyja gracza + offset
-        transform.position = player.position + cameraOffset;
+        //transform.position = player.position + cameraOffset;
+
+
+
+        //bardziej kulturalnie - lerp
+        //policz pozycje docelow¹
+        Vector3 targetPosition = player.position + cameraOffset;
+        
+        //u¿yj interpolacji liniowej
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+        
+        //u¿yj smooothdamp
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraSpeed, smoothTime);
     }
 }
