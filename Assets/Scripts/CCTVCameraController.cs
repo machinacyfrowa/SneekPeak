2fdced44f156b696bf1585a84d80487f927b35dd
pzawadzki <pzawadzki@TE-GDA-D-D36G18>
2024-02-28 15:13:02 +0100

    raycast out of camera

--- Assets/Scripts/CCTVCameraController.cs
+++ Assets/Scripts/CCTVCameraController.cs
@@ -14,10 +14,14 @@ public class CCTVCameraController : MonoBehaviour
     //czy kamera krêci siê w prawo
     bool turningRight = !true;
 
+    //obiektyw kamery
+    Transform cameraLens;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform cameraPosition = transform.Find("CameraPosition");
+        cameraLens = cameraPosition.Find("Cylinder");
     }
 
     // Update is called once per frame
@@ -27,6 +31,13 @@ public class CCTVCameraController : MonoBehaviour
         // oscyluj¹ce pomiêdzy 0 a 9, a nastêpnie mno¿ymy to przez 10 ¿eby uzyskaæ szybszy ruch
         // i na koniec odejmujemy od wartoœci otrzymanej 45 stopni aby uzyskaæ ruch w zakresie -45 do 45
         transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9)*10-45, 0));
+
+        CheckIfPlayerVisible();
+    }
+
+    void CheckIfPlayerVisible()
+    {
+        Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down)*100, Color.yellow);
     }
 
 }
